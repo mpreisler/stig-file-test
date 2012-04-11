87b65943528aab8b5e065d814328763ddde0ee9e
Martin Preisler <mpreisle@redhat.com>
2012-04-11 16:53:02 +0200

    Fixed and tidied up copyright clauses

--- converter.py
+++ converter.py
@@ -17,9 +17,28 @@ def create_check(comment_line, lines):
     check_name, description = rest.split(" ", 1)
     
     with open("output/stig-GEN00%s.sh" % (check_name), "w") as f:
-        f.write("#!/usr/bin/env bash\n")
-        f.write("\n")
-        f.write("# Generated from content by Steve Grubb, GPLv2+\n")
+        f.write("#!/usr/bin/env sh\n")
+        f.write("""
+# This file is based on STIG file permission script by Steve Grub
+#
+# Copyright (c) 2011 Steve Grubb. ALL RIGHTS RESERVED.
+# sgrubb@redhat.com
+#
+# This software may be freely redistributed under the terms of the GNU
+# public license version 2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# In Feb 2011, DISA released UNIX Security Checklist Version 5,
+# Release 1.28 to match with the OS SRG Unix.
+# http://iase.disa.mil/stigs/downloads/zip/u_unix_v5r1-28_checklist_20110128.zip
+# http://iase.disa.mil/stigs/downloads/zip/unclassified_os-srg-unix_v1r1_finalsrg.zip
+#
+# Modified by Martin Preisler <mpreisle@redhat.com> for use with XCCDF + SCE
+""")
+
         f.write("#\n")
         f.write("# original description: %s\n" % (description))
         f.write("\n")
--- output/stig-GEN002700.sh
+++ output/stig-GEN002700.sh
@@ -1,6 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-# Generated from content by Steve Grubb, GPLv2+
+# This file is based on STIG file permission script by Steve Grub
+#
+# Copyright (c) 2011 Steve Grubb. ALL RIGHTS RESERVED.
+# sgrubb@redhat.com
+#
+# This software may be freely redistributed under the terms of the GNU
+# public license version 2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# In Feb 2011, DISA released UNIX Security Checklist Version 5,
+# Release 1.28 to match with the OS SRG Unix.
+# http://iase.disa.mil/stigs/downloads/zip/u_unix_v5r1-28_checklist_20110128.zip
+# http://iase.disa.mil/stigs/downloads/zip/unclassified_os-srg-unix_v1r1_finalsrg.zip
+#
+# Modified by Martin Preisler <mpreisle@redhat.com> for use with XCCDF + SCE
 #
 # original description: all audit logs must be 0640 or less
 
